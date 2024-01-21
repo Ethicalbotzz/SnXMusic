b5ec93f6474c91da685bdcf55b5b95482124bd6d
Ethicalbotzz <156450212+Ethicalbotzz@users.noreply.github.com>
2024-01-21 22:23:08 +0530

    Update config.py
    Signed-off-by: Ethicalbotzz <156450212+Ethicalbotzz@users.noreply.github.com>

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MAUTMOODDSS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MAUTCHAT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BIGDADDYHUB11")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DADDYHUB11")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/Thanos-01-08-8"
+    "START_IMG_URL", "https://telegra.ph/Devil-01-21-15"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
